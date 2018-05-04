106803360a95489977074735408b101cd019328b
Sebastian Fix <sebastian.fix@smartgate.ch>
2018-05-04 12:00:25 +0200

    Updated Kerne

--- app/App/Console/Kernel.php
+++ app/App/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
     ];
 
     /**
@@ -25,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('sync:newsletter')->hourly();
+        $schedule->command('sync:newsletters')->hourly();
     }
 
     /**
