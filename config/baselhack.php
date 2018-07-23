15e4ff6e421c387e90a4c2a2c727dc1d7502b607
Stan Barrows <stan.barrows@smartgate.ch>
2018-07-23 16:32:38 +0000

    Apply fixes from StyleCI

--- config/baselhack.php
+++ config/baselhack.php
@@ -15,7 +15,7 @@ return [
 
     'notifications' => [
         'signup' => ['event@baselhack.ch'],
-        'contact_form' => ['sponsoring@baselhack.ch','event@baselhack.ch','info@baselhack.ch'],
+        'contact_form' => ['sponsoring@baselhack.ch', 'event@baselhack.ch', 'info@baselhack.ch'],
     ],
 
     'socials' => [
@@ -84,5 +84,5 @@ return [
             'image' => 'swiss_made_software.png',
             'url' => 'https://www.swissmadesoftware.org',
         ],
-    ]
+    ],
 ];
