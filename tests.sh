47d481c44966b23957e35d3ccc353c628b54533a
Sebastian Fix <sebastian.fix@onicial.ch>
2018-09-03 22:15:44 +0000

    Removed Auto Deployment

--- tests.sh
+++ tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -s 'https://forge.laravel.com/servers/164367/sites/514711/deploy/http?token=Z30vEiuNQzdvpA0mDjeCpCGTsoAbYxhkciOfVFQ0';
+curl -s '';
 
 echo 'Deployment triggered!
